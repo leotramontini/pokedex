03328db1813b1fb150ab6d50c0780dba0eea7431
Leonardo Tramontini <leo.tramontini@gmail.com>
2020-08-12 21:40:34 -0300

    Create route and base components and page

---
+++ src/components/MenuItem/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Text = styled.div`
+  width: 100px;
+  margin: 10px;
+  background-color: red;
+`
---
+++ src/pages/Menu/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { Container } from './styles'
+import MenuItem from '../../components/MenuItem'
+
+const Menu: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+      </Container>
+    </>
+  )
+}
+
+export default Menu
